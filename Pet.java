5b743dd6b92c6713f65ce30b306f6ba526a6a4c5
Micheline-gif <michelinecineus0@gmail.com>
2025-09-05 22:42:22 -0400

    Created a Pet class

---
+++ Pet.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Pet here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Pet
+{
+    // instance variables - replace the example below with your own
+    private String name;
+
+    /**
+     * Constructor for objects of class Pet
+     */
+    public Pet(String petsName)
+    {
+         
+        name = petsName;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public String sampleMethod(int y)
+    {
+        // put your code here
+        return name + y;
+    }
+}
